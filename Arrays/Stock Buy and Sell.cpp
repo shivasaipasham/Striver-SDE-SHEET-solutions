d306a3a9109b8b35544b0b6bf9e3dc943dfc0b3c
shivasaipasham <68676319+shivasaipasham@users.noreply.github.com>
2021-06-29 09:56:04 +0530

    Create Stock Buy and Sell.cpp

---
+++ Arrays/Stock Buy and Sell.cpp
@@ -0,0 +1,22 @@
+/* Problem Link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Approach 1: Brute force (Using two  loops)
+            Time Complexity: O(n²)
+            Space Complexity:O(1)
+            
+Approach 2: Keep track of minimum element on left and update profit value 
+            Time Complexity: O(n)
+            Space Complexity:O(1)
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(),mini = INT_MAX,ans=0;
+        for(int i=0;i<n;i++){
+            mini = min(prices[i],mini);
+            ans = max(ans,prices[i]-mini);
+        }
+        return ans;
+    }
+};
